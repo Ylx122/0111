c068ab9df479313b1a235d83270a18bc912e1d16
yfl410ylx <1695421675@qq.com>
2021-01-11 08:57:03 +0800

    增加.md 完善代码

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,18 @@
 public class Student {
+    private String major;
+    private String phone;
+
+    public Student(String major, String phone){
+        this.major = major;
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "major='" + major + '\'' +
+                "password='" + super.getPassword(0) + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
 }
---
+++ src/Test02.java
@@ -0,0 +1,6 @@
+public class Test02 {
+    public static void main(String[] args) {
+        Student stu = new Student("网络工程","12350506498");
+        System.out.println(stu);
+    }
+}
